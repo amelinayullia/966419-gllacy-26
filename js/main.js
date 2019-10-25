020fa8feb91323d66338c9772c14b2c6f14298e3
Amelina_yuliia <amelinayullia@gmail.com>
2019-10-25 23:04:08 +0300

    Оживляет форму модального окна с помощью JS

---
+++ js/main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+var link = document.querySelector(".button--feedback");
+var bodyElement = document.querySelector("body");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-button-close");
+var form = popup.querySelector("form");
+var userName = popup.querySelector("[name=user-name]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  userName.focus();
+  bodyElement.classList.remove("popup-hidden");
+  bodyElement.classList.add("popup-open");
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+  bodyElement.classList.remove("popup-open");
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
